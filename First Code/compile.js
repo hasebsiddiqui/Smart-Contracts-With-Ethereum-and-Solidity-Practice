601bceb2e226655c4535d36b7f2acc6d71f795e8
Haseeb Ahmad <43096403+hasebsiddiqui@users.noreply.github.com>
2022-01-06 16:52:04 +0500

    First contract testing architecture

--- First Code/compile.js
+++ First Code/compile.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts','Inbox.sol');
 const source = fs.readFileSync(inboxPath,'utf8');
+console.log(solc.compile(source,1).contracts[':Inbox']);
 
-console.log(solc.compile(source,1));
