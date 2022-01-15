4256807db6169d946664ce7c7d54022db85abe48
Haseeb Ahmad <43096403+hasebsiddiqui@users.noreply.github.com>
2022-01-16 04:01:20 +0500

    Update deploy.js

--- First Code/deploy.js
+++ First Code/deploy.js
@@ -1,1 +1,23 @@
 // deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("Web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider("", "");
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Deploying contract using address: ", accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there"],
+    })
+    .send({ gas: 1000000, from: accounts[0] });
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
